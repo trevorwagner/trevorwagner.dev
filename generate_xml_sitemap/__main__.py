02968e97b86b255657c0ff87b1c3db190f13786a
Trevor Wagner <trevorwagner.dev@gmail.com>
2023-12-14 18:37:29 -0600

    completes module to generate XML sitemap from manifest

---
+++ generate_xml_sitemap/__main__.py
@@ -0,0 +1,20 @@
+from pathlib import Path
+import json
+from build_sitemap_from_manifest import build_sitemap_from_manifest
+
+DIST = Path(__file__).parent.resolve() / '../_dist/'
+site_manifest_file = DIST / 'site-manifest.json'
+
+xml_sitemap_file = DIST / 'html/sitemap.xml'
+
+
+if __name__ in '__main__':
+    with open(site_manifest_file) as m:
+        sitemap = build_sitemap_from_manifest(json.load(m))
+
+        parent_folder = Path(xml_sitemap_file.parent)
+        if not parent_folder.exists():
+            parent_folder.mkdir(parents=True, exist_ok=True)
+
+        with open(xml_sitemap_file, 'w') as f:
+            f.write(sitemap)
---
+++ generate_xml_sitemap/build_sitemap_from_manifest.py
@@ -0,0 +1,25 @@
+from datetime import datetime, timezone
+
+
+def build_sitemap_from_manifest(manifest):
+    sitemap = ''.join([
+        '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">',
+    ])
+
+    # 2023-12-15T00:20:38.657Z
+    for entry in manifest['site']:
+        timestamp = (datetime.fromtimestamp(entry['file']['lastMod'], tz=timezone.utc)
+                     .strftime('%Y-%m-%dT%H:%M:%S.%fZ'))
+
+        new_item = ''.join([
+            '\n\t<url>',
+            '\n\t\t<loc>https://trevorwagner.dev{}</loc>'.format(entry['page']['relativePath']),
+            '\n\t\t<lastmod>{}</lastmod>'.format(timestamp),
+            '\n\t</url>'
+        ])
+
+        sitemap = ''.join([sitemap, new_item])
+
+    sitemap = ''.join([sitemap, '\n</urlset>'])
+
+    return sitemap
