7d3bbca68afa025b63ec4ec852ede5700f75b826
Trevor Wagner <trevorwagner.dev@gmail.com>
2023-12-15 17:52:28 -0600

    Adds umami script (commented)

--- generate_html/build_html_for_entry.py
+++ generate_html/build_html_for_entry.py
@@ -18,6 +18,7 @@ def build_html_for_entry(entry, matter, content):
             for key, value in assemble_opengraph_data_for_entry(entry, matter, content).items():
                 a.meta(property=key, content=value)
             a.title(_t='{} | Trevor Wagner'.format(escape(entry['page']['title'])))
+            # a('<script async src="https://us.umami.is/script.js" data-website-id="72e1cfab-c988-430b-9f25-1f52cf8720f4"></script>')
             a.script(src="https://cdn.tailwindcss.com")
             if entry['page']['type'] == 'blogPost':
                 a.script(src="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.9.0/highlight.min.js")
