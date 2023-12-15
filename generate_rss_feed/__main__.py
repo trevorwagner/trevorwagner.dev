b09074a60774495fa2b0b748f9f223d8bb4f3de0
Trevor Wagner <trevorwagner.dev@gmail.com>
2023-12-14 18:38:00 -0600

    Updates rss feed module for consistency

--- generate_rss_feed/__main__.py
+++ generate_rss_feed/__main__.py
@@ -4,9 +4,9 @@ from build_rss_from_manifest import build_rss_from_inventory
 from pathlib import Path
 
 DIST = Path(__file__).parent.resolve() / '../_dist/'
+site_manifest_file = DIST / 'site-manifest.json'
 
 rss_xml_file = DIST / 'html/blog/feed/rss.xml'
-site_manifest_file = DIST / 'site-manifest.json'
 
 
 if __name__ in '__main__':
@@ -14,9 +14,9 @@ if __name__ in '__main__':
     with open(site_manifest_file) as m:
         rss = build_rss_from_inventory(json.load(m))
 
-        feed_folder = Path(rss_xml_file.parent)
-        if not feed_folder.exists():
-            feed_folder.mkdir(parents=True, exist_ok=True)
+        parent_folder = Path(rss_xml_file.parent)
+        if not parent_folder.exists():
+            parent_folder.mkdir(parents=True, exist_ok=True)
 
         with open(rss_xml_file, 'w') as f:
             f.write(rss)
