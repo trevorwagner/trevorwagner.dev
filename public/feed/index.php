b679f6c55b857b688f95d24809c58ccbf2858662
Trevor Wagner <trevorwagner.dev@gmail.com>
2023-12-17 01:17:38 -0600

    Fixes php for feed

--- public/feed/index.php
+++ public/feed/index.php
@@ -1,11 +1,10 @@
 <?php
-    $attachment_location = $_SERVER["DOCUMENT_ROOT"] . "/blog/feed/rss.xml";
+    $attachment_location = $_SERVER['DOCUMENT_ROOT']."/blog/feed/rss.xml";
 
     if (file_exists($attachment_location)) {
-
-        header($_SERVER["SERVER_PROTOCOL"] . " 200 OK");
+        header($_SERVER["SERVER_PROTOCOL"]." 200 OK");
         header("Cache-Control: public"); // needed for internet explorer
-        header("Content-Type: application/rss+xml");
+        header("Content-type: application/rss+xml");
         header("Content-Transfer-Encoding: Binary");
         header("Content-Length:".filesize($attachment_location));
         header("Content-Disposition: attachment; filename=rss.xml");
@@ -13,5 +12,6 @@
         die();
     } else {
         http_response_code(404);
-        die()
+        die();
+    }
 ?>
