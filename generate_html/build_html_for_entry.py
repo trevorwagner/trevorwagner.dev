592c360166ffb7313c396242d1118b7d27da7817
Trevor Wagner <trevorwagner.dev@gmail.com>
2023-12-19 17:23:26 -0600

    Adds meta tag for mobile viewports to HTML

--- generate_html/build_html_for_entry.py
+++ generate_html/build_html_for_entry.py
@@ -19,6 +19,7 @@ def build_html_for_entry(entry, matter, content):
             a.meta(charset="utf-8")
 
             a.meta(name='author', content='Trevor Wagner')
+            a.meta(name="viewport", content="width=device-width, initial-scale=1.0")
             for key, value in assemble_opengraph_data_for_entry(entry, matter, content).items():
                 a.meta(property=key, content=value)
 
