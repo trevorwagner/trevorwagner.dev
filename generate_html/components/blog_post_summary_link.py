ff248403327b80df6f9f3636ee2a594a7af5de2b
Trevor Wagner <trevorwagner.dev@gmail.com>
2024-01-09 22:33:50 -0600

    Updates blog_post_summary_link not to wrap everything in an <a>

--- generate_html/components/blog_post_summary_link.py
+++ generate_html/components/blog_post_summary_link.py
@@ -6,9 +6,9 @@ from .blog_post_pub_date import blog_post_pub_date
 
 def blog_post_summary_link(a: Airium, entry):
     with a.div(klass="post-summary"):
+        a.img(src=entry['coverPhoto']['thumbnail'])
         with a.a(href=entry['page']['relativePath']):
-            a.img(src=entry['coverPhoto']['thumbnail'])
             a.h2(_t=escape(entry['page']['title']))
-            a.p(_t=escape(blog_post_pub_date(entry['page']['publishDate'])))
-            a.div(klass="clear-both")
+        a.time(_t=escape(blog_post_pub_date(entry['page']['publishDate'])))
+        a.div(klass="clear-both")
     return
