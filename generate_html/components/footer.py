522109c5f5adf704184d596eed26590e0e27ef9e
Trevor Wagner <trevorwagner.dev@gmail.com>
2024-01-11 13:32:40 -0600

    Footer now uses <hr/> for top border

--- generate_html/components/footer.py
+++ generate_html/components/footer.py
@@ -5,6 +5,7 @@ from datetime import datetime
 
 def footer(a: Airium):
     with a.footer():
+        a.hr()
         a.p(_t="&#169; {} Upstream Consulting LLC. All Rights Reserved.".format(datetime.now().strftime('%Y')))
 
     return
