c2a8f9336d9b29f10795987eb23f3f8cb55a75c4
Trevor Wagner <trevorwagner.dev@gmail.com>
2024-01-11 21:30:09 -0600

    Adds Umami to site (mmmmm)

--- generate_html/build_html_for_entry.py
+++ generate_html/build_html_for_entry.py
@@ -1,12 +1,11 @@
 from airium import Airium
 
-from .assemble_opengraph_data import assemble_opengraph_data_for_entry
+from assemble_opengraph_data import assemble_opengraph_data_for_entry
 
-from .components.footer import footer
-from .components.header import header
-from .components.overlay_menu import overlay_menu
-from .components.page_content import page_content
-from .components.page_title import page_title
+from components.page_content import page_content
+from components.header import header
+from components.footer import footer
+from components.page_title import page_title
 
 
 def build_html_for_entry(entry, matter, content):
@@ -20,28 +19,30 @@ def build_html_for_entry(entry, matter, content):
             a.meta(charset="utf-8")
 
             a.meta(name='author', content='Trevor Wagner')
-            a.meta(name="viewport", content="width=device-width, initial-scale=1.0")
             for key, value in assemble_opengraph_data_for_entry(entry, matter, content).items():
                 a.meta(property=key, content=value)
 
             page_title(a, entry)
-            # a('<script async src="https://us.umami.is/script.js" data-website-id="72e1cfab-c988-430b-9f25-1f52cf8720f4"></script>')
+            a('<script async src="https://us.umami.is/script.js" data-website-id="72e1cfab-c988-430b-9f25-1f52cf8720f4"></script>')
 
-            a.link(rel="stylesheet", href="/css/styles.css")
+            a.script(src="https://cdn.tailwindcss.com")
 
             if entry['page']['type'] == 'blogPost':
                 a.script(src="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.9.0/highlight.min.js")
                 a.link(rel="stylesheet",
                        href="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.9.0/styles/base16/default-dark.css")
-                
-        with a.body():
-            header(a, entry)
-            overlay_menu(a, entry)
-            with a.div():
-                page_content(a, entry, content)
-                footer(a)
 
-            a.script(type="text/javascript", src="/js/slidedown-menu.js")
+        with a.body(klass="subpixel-antialiased"):
+            header(a, entry)
+
+            with a.div(klass="lg:ml-96 px-16 height-full max-w-6xl"):
+
+                with a.div():
+                    page_content(a, entry, content)
+
+                    a.div(klass="clear-both")
+
+                footer(a)
             a.script(_t="hljs.highlightAll();")
 
     return str(a)
