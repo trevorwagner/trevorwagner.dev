3fad75ccb9a4fdc58ec02e01e094f086c370e0eb
Trevor Wagner <trevorwagner.dev@gmail.com>
2024-03-29 19:25:01 -0500

    Adds code to insert blog posts to DB

--- collect_inventory/__main__.py
+++ collect_inventory/__main__.py
@@ -1,6 +1,6 @@
 from sqlalchemy.orm import Session
 
-from _static import list_page_files
+from _static import list_page_files, list_post_files
 from collect_inventory.builders.md_file_record_builder import build_md_file_record
 from collect_inventory.builders.page_record_builder import build_page_record
 from site_data_service import engine, init_db
@@ -10,6 +10,7 @@ if __name__ == '__main__':
 
   with Session(engine) as session:
 
+    # Add brochure pages in any order.
     for file in list_page_files():
       md_file = build_md_file_record(file)
 
@@ -19,3 +20,19 @@ if __name__ == '__main__':
 
         session.add(page_record)
         session.commit()
+
+    # Add Blog posts, sorted in order of publicationDate.
+    blog_posts = []
+
+    for file in list_post_files():
+      md_file = build_md_file_record(file)
+
+      with open(file) as f:
+        page_record = build_page_record(file, f.read())
+        page_record.md_file = md_file
+
+        blog_posts.append(page_record)
+
+    blog_posts_sorted = sorted(blog_posts, key=lambda x: x.published)
+    session.add_all(blog_posts_sorted)
+    session.commit()
