35e23c12964243efdaa52207e1cad2ac3dbf266d
Trevor Wagner <trevorwagner.dev@gmail.com>
2024-06-24 16:58:22 -0500

    Updates sanitize script to include dash, bold, italic

--- _sanitize_markdown.sh
+++ _sanitize_markdown.sh
@@ -3,16 +3,16 @@
 printf $'Removing smart characters and unnecessary formatting from markdown files...\n'
 
 function replace_unwanted_characters_in_file() {
-    sanitized_md=$( cat ${1}            \
-    | sed  s/'\xe2\x80\x98'/\'/g        \
-    | sed  s/'\xe2\x80\x99'/\'/g        \
-    | sed  s/'\xe2\x80\x9c'/\"/g        \
-    | sed  s/'\xe2\x80\x9d'/\"/g        \
-    | sed  s/'\xe2\x80\x94'/--/g        \
-    | sed  s/'\xe2\x80\xa6'/.../g       \
-    | sed -e 's/\([.?!]\) \{2,\}/\1 /g' \
-    | sed -e s/' \{1,\}$//g'            \
-    | grep -A1 . - | grep -v "^--$"     \
+    sanitized_md=$( cat ${1}              \
+    | sed  s/'\xe2\x80\x98'/\'/g          \
+    | sed  s/'\xe2\x80\x99'/\'/g          \
+    | sed  s/'\xe2\x80\x9c'/\"/g          \
+    | sed  s/'\xe2\x80\x9d'/\"/g          \
+    | sed  s/'\xe2\x80\x94'/--/g          \
+    | sed  s/'\xe2\x80\xa6'/.../g         \
+    | sed -e 's/\([.?!*-]\) \{2,\}/\1 /g' \
+    | sed -e s/' \{1,\}$//g'              \
+    | grep -A1 . - | grep -v "^--$"       \
     )
     printf "%s" "${sanitized_md}" > ${1}
 }
